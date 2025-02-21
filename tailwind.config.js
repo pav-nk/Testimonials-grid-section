b6bccab5f78a5fdc818dab3de695eeea1c0c85e2
pav_nk <pav_nk@vk.com>
2025-02-21 16:57:05 +0300

    feat: completed

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {
+      backgroundPosition: {
+        'right-top': 'right 2rem top',
+      },
+      colors(theme) {
+        return {
+          primary: {
+            DEFAULT: 'hsl(263, 55%, 52%)',
+            dark: 'hsl(217, 19%, 35%)',
+            blackish: 'hsl(219, 29%, 14%)',
+          },
+          gray: {
+            ...theme.colors.gray,
+            100: 'hsl(0, 0%, 81%)',
+            200: 'hsl(210, 46%, 95%)',
+          },
+        };
+      },
+    },
+  },
+  plugins: [],
+};
